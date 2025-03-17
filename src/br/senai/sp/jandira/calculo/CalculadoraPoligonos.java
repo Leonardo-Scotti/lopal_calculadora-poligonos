f336fa878e96165885fcaa3d61d6c40e8a72fd9f
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-03-17 14:27:53 -0300

    Criação da classe Menu e do método criarMenu

--- src/br/senai/sp/jandira/calculo/CalculadoraPoligonos.java
+++ src/br/senai/sp/jandira/calculo/CalculadoraPoligonos.java
@@ -7,29 +7,32 @@ import br.senai.sp.jandira.calculo.model.Quadrado;
 import br.senai.sp.jandira.calculo.model.Retangulo;
 import br.senai.sp.jandira.calculo.model.Trapezio;
 import br.senai.sp.jandira.calculo.model.Triangulo;
+import br.senai.sp.jandira.calculo.ui.Menu;
 
 public class CalculadoraPoligonos {
 	
 	public static void main(String[] args) {
 		
-		//Criação dos objetos
-		//Quadrado
-		Quadrado quadrado1 = new Quadrado();
+		Menu.criarMenu();
 		
-		Scanner leitor = new Scanner(System.in);
-		
-		
-		System.out.print("Digite o valor do lado do quadrado: ");
-		double lado = leitor.nextDouble();
-		leitor.nextLine();
-		
-		System.out.print("Qual é o seu nome? ");
-		String nome = leitor.nextLine();
-		
-		System.out.println("Olá, " + nome);
-		
-		quadrado1.setLado(lado);
-		quadrado1.exibirDados();
+//		//Criação dos objetos
+//		//Quadrado
+//		Quadrado quadrado1 = new Quadrado();
+//		
+//		Scanner leitor = new Scanner(System.in);
+//		
+//		
+//		System.out.print("Digite o valor do lado do quadrado: ");
+//		double lado = leitor.nextDouble();
+//		leitor.nextLine();
+//		
+//		System.out.print("Qual é o seu nome? ");
+//		String nome = leitor.nextLine();
+//		
+//		System.out.println("Olá, " + nome);
+//		
+//		quadrado1.setLado(lado);
+//		quadrado1.exibirDados();
 	
 	}
 
---
+++ src/br/senai/sp/jandira/calculo/ui/Menu.java
@@ -0,0 +1,19 @@
+package br.senai.sp.jandira.calculo.ui;
+
+public class Menu {
+	
+	public static void criarMenu() {
+		System.out.println("=========================");
+		System.out.println("CALCULADORA DE POLÍGONOS");
+		System.out.println("------------------------");
+		System.out.println("1 - QUADRADO");
+		System.out.println("2 - RETÂNGULO");
+		System.out.println("3 - TRIÂNGULO");
+		System.out.println("4 - TRAPÉZIO");
+		System.out.println("5 - CIRCUNFERÊNCIA");
+		System.out.println("------------------------");
+		System.out.println("Escolha uma opção (1-5): ");
+		System.out.println("=========================");
+	}
+	
+}
