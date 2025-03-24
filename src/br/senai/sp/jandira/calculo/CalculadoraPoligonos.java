2caf564f2fd755ccbd518f5bbae3f2b4ff112bd4
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-03-24 16:51:42 -0300

    Treinando Loopings

--- src/br/senai/sp/jandira/calculo/CalculadoraPoligonos.java
+++ src/br/senai/sp/jandira/calculo/CalculadoraPoligonos.java
@@ -1,14 +1,37 @@
 package br.senai.sp.jandira.calculo;
 
+import java.util.Scanner;
+
 import br.senai.sp.jandira.calculo.ui.Menu;
 
 public class CalculadoraPoligonos {
-	
+
 	public static void main(String[] args) {
-		Menu.criarMenu();
-		
-		System.out.println("Programa Finalizado! Reinicie para calcular novamente.");
-		System.out.println("=========================");
+
+		// Looping / Laços de repetição
+//		int contador = 1;
+//		while(contador <= 10) {
+//			System.out.println("SENAI JANDIRA! - " + contador);
+//			contador = contador + 1;
+//		}
+//		
+//		String resposta = "s";
+//		while (resposta.equals("s")) {
+//			System.out.println("Estamos dando voltas!");
+//			System.out.println("Gostaria de dar mais uma volta (s/n)?");
+//			Scanner leitor = new Scanner(System.in);
+//			resposta = leitor.next();
+//		}
+
+		System.out.println("- TABUADA DO 9 -");
+		int i = 0;
+		while (i <= 10) {
+			int resultado = 9 * i;
+			System.out.println("9 x " + i + " = " + resultado);
+			i++;
+		}
+
+		// Menu.criarMenu();
 	}
 
 }
